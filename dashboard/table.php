f47d6d59bd53b3f0be9284a10c452260402d64d2
briankihia <brianjohnkiama@gmail.com>
2024-10-09 10:40:19 +0300

    changed the icons for postedjob, addjobs and applicants

--- dashboard/table.php
+++ dashboard/table.php
@@ -46,7 +46,7 @@
         <div class="sidebar pe-4 pb-3">
             <nav class="navbar bg-light navbar-light">
                 <a href="index.html" class="navbar-brand mx-4 mb-3">
-                    <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>DASHMIN</h3>
+                    <h3 class="text-primary">ADMIN</h3>
                 </a>
                 <div class="d-flex align-items-center ms-4 mb-4">
                     <div class="position-relative">
@@ -59,9 +59,10 @@
                     </div>
                 </div>
                 <div class="navbar-nav w-100">
-                    <a href="index.php" class="nav-item nav-link"><i class="fa fa-tachometer-alt me-2"></i>POSTED JOBS</a>
-                    <a href="add_jobs.html" class="nav-item nav-link "><i class="fa fa-tachometer-alt me-2"></i>ADD JOBS</a>
-                    <a href="table.php" class="nav-item nav-link active"><i class="fa fa-table me-2"></i>APPLICANTS</a>  <!--Tables -->
+                    <!-- <a href="index.php" class="nav-item nav-link"><i class="fa fa-tachometer-alt me-2"></i>POSTED JOBS</a> -->
+                    <a href="index.php" class="nav-item nav-link"><i class="fa fa-briefcase me-2"></i>POSTED JOBS</a>
+                    <a href="add_jobs.html" class="nav-item nav-link"> <i class="fa fa-plus me-2"></i>ADD JOBS</a>
+                    <a href="table.php" class="nav-item nav-link"> <i class="fa fa-users me-2"></i>APPLICANTS</a>  <!--Tables -->
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-laptop me-2"></i>Elements</a>
                         <div class="dropdown-menu bg-transparent border-0">
